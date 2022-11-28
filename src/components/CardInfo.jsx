dc0449ee49300db1b7699a6d3a32f2298bd59751
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-11-28 12:14:51 +0100

    proyecto github

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -1,45 +1,52 @@
 import React from 'react'
 
 export const CardInfo=(props)=>{
+    
     return(
         <>
-            <div className="card_avatarUrl">
-
-            </div>
-            <div className="card_info">
-                <div className="card_login">
+            <div className="card_containerInfo">
+                <div className="card_avatarUrl">
+                    <img src={props.info.avatar_url}  alt="" id="card_imagen"></img>
+                    {console.log("componente cardinfo: "+props.style)}
+                </div>
+                <div className="card_info">
                     <div className="card_username">
-                        <h1>The Octocat</h1>
+                        <h6>{props.info.name}</h6>
+                    </div>
+                    <div className="card_login2">
+                        <p>{props.info.login? "@"+props.info.login:""}</p>
                     </div>
                     <div className="card_date">
-                        <p>Fecha</p>
+                        <p>{props.info.created_at}</p>
                     </div>
                 </div>
-                <div className="card_login2">
-                    <p>@octocat</p>
-                </div>
-                <div className="bio">
-                    <p>This profile has no bio</p>
-                </div>
+            </div>
+            <div className="bio">
+                {props.info.bio===null ? <p>This profile has no bio</p> : <p>{props.info.bio}</p>}
+            </div>
+            <div className="card_containerInfo2">
                 <div className="card_container_info1">
-                    <div className="card_repos">Repos</div>
-                    <div className="card_followers">Followers</div>
-                    <div className="card_following">Following</div>
+                    <div className="card_repos">{props.info.public_repos? <p>Repos</p>:""}</div>
+                    <div className="card_followers">{props.info.followers? <p>Followers</p>:""}</div>
+                    <div className="card_following">{props.info.following? <p>Following</p>:""}</div>
                 </div>
                 <div className="card_container_info">
-                    <div className="card_repos_url"></div>
-                    <div className="card_followers_url"></div>
-                    <div className="card_following_url"></div>
-                </div>
-                <div className="card_info2">
-                    <div className="city"><p>San Francisco</p></div>
-                    <div className="twitter">Not avariable</div>
-                </div>
-                <div className="card_info3">
-                    <div className="url"><p>https://github.blog</p></div>
-                    <div className="company">agithub</div>
+                    <div className="card_repos_url">{props.info.public_repos}</div>
+                    <div className="card_followers_url">{props.info.followers}</div>
+                    <div className="card_following_url">{props.info.following}</div>
                 </div>
             </div>
+            <div className='card_containerInfo3'>
+                <div className="card_info2">
+                    <div className="city">{props.info.city}</div>
+                    <div className="twitter">{props.info.twitter_username===null ? <p>Not Avariable</p>: <p>{props.info.twitter_username}</p>}</div>
+                </div>
+                <div className="card_info3">
+                    <div className="url"><p>{props.info.url}</p></div>
+                    <div className="company">{props.info.company}</div>
+                </div>
+            </div>
+            
         </>
         
     )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,10 @@
-import React,{useState, useEffect} from 'react'
+import React,{useState} from 'react'
 import { CardData } from './CardData'
 import useFetch from './hooks/useFetch'
 
 export const SearchBar=(props)=>{
     const [dato,setDato]=useState("")
-
+    
     const handlerChange=(e)=>{
         setDato(e.target.value)
         console.log(dato)
@@ -13,17 +13,20 @@ export const SearchBar=(props)=>{
     //Pasamos los datos resultantes de useFetch al componente hijo que es CardData donde se muestran los resultados de la consulta
     return(
         <div className="card_container">
-            <div className="card_search">
+            <div className={props.style?"card_searchLight":"card_searchDark"}>
                 <div className="card_input">
-                    <input onChange={handlerChange}  placeholder='Search Github username...' id="searchBar"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#0378FC" className="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>&nbsp;
+                        <input onChange={handlerChange}  placeholder='Search Github username...' id={props.style?"searchBarLight":"searchBarDark"}/>
                 </div>
                 <div className="card_button">
-                    <button onClick={getData}>Search</button>
+                    <button onClick={getData} className="btn btn-primary " id="buttonSearch">Search</button>
                 </div>
                 
             </div>
-            <div className="card_data">
-                <CardData isLoading={isLoading} info={info} error={error}></CardData>
+            <div className={props.style?"card_dataLight":"card_dataDark"}>
+                <CardData isLoading={isLoading} info={info} style={props.style} error={error} ></CardData>
             </div>
         </div>
     )
